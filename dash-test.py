bf048bb76843b9f13be10c77751321b8e53ac02e
Mingyyy <j.yanming@gmail.com>
2019-09-13 16:38:24 -0400

    Update strategy.py; test out user input ticker and plot stock open and close for a given period.

--- dash-test.py
+++ dash-test.py
@@ -1,9 +1,79 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+from datetime import datetime as dt
+
 
 app = dash.Dash()
-app.layout = html.Div("MVP - Upgrade")
+app.layout = html.Div(children=[
+    html.H1("MVP - Time to Upgrade"),
+    html.Div(children='''
+    symbol to graph:'''),
+    dcc.Input(id='input', value='', type='text'),
+    html.Div(id='output-graph'),]
+)
+
+@app.callback(
+    Output(component_id='output-graph',component_property='children'),
+    [Input(component_id='input',component_property='value')]
+    )
+def update_graph(input_data):
+    path = "./daily-historical-stock-prices-1970-2018/"
+
+    df_price = pd.read_csv(path + 'historical_stock_prices.csv', nrows=200)
+    # df_stock = pd.read_csv(path + 'historical_stocks.csv')
+    selected_ticker = input_data
+    selected_col1 = 'open'
+    selected_col2 = 'close'
+
+    list_open = df_price[selected_col1][df_price['ticker'] == selected_ticker]
+    list_close = df_price[selected_col2][df_price['ticker'] == selected_ticker]
+    list_date = df_price['date'][df_price['ticker'] == selected_ticker]
+
+    return dcc.Graph(id='example',
+              figure={
+                  'data':[{'x':list_date,
+                           'y':list_open,
+                           'type':'line',
+                           'name': selected_col1},
+                          {'x': list_date,
+                           'y': list_close,
+                           'type': 'bar',
+                           'name': selected_col2}
+                          ],
+                  'layout':{
+                      'title': f'{selected_ticker} historical data'
+                  }
+
+    }),
+
+
+
+def s_buy_hold(stocks, df_price, start_date='2014-05-08', end_date='2014-05-14', invest=100, frequency=30):
+    # strategy 1. buy invest amount at start_date, calculate the PnL at end_date
+    pnl = 0
+    if dt.strptime(end_date, "%Y-%m-%d") <= dt.strptime(start_date, "%Y-%m-%d"):
+        return 'You end date is before your start date!'
+    if len(stocks) == 0:
+        return 'You stock ticker is out of scope.'
+    for stock in stocks:
+        select = df_price[df_price['ticker'] == stock][['date', 'adj_close']].copy()
+        if start_date in select['date'].values:
+            p0=select['adj_close'][select['date']==start_date].values[0]
+            if p0 == 0:
+                return "Starting price is zero."
+            else:
+                q = invest/p0
+        else:
+            return 'start date or ticker doesn\'t exist'
+        if end_date in select['date'].values:
+            p1=select['adj_close'][select['date']==end_date].values[0]
+            pnl = q * (p1 - p0)
+        else:
+            return 'end date out of scope.'
+    return pnl
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- strategy.py
+++ strategy.py
@@ -1,38 +1,68 @@
 import boto3, os, sys
 import pandas as pd
+from datetime import datetime as dt
 
-# connect to S3
-s3 = boto3.client('s3')
-resource = boto3.resource('s3') #high-level object-oriented API
-
+# connect to S3, with boto3: high-level object-oriented API
+# s3 = boto3.client('s3')
+# resource = boto3.resource('s3')
 
 # connects to S3 using the default profile credentials and lists all the S3 buckets
 # buckets = s3.list_buckets()
 # for bucket in buckets['Buckets']:
 #     print(bucket['CreationDate'].ctime(), bucket['Name'])
 
-# my_bucket = resource.Bucket('testing_ming')
-# # download file into current directory
-# for s3_object in my_bucket.objects.all():
-#     # Need to split s3_object.key into path and file name, else it will give error file not found.
-#     path, filename = os.path.split(s3_object.key)
-#     my_bucket.download_file(s3_object.key, filename)
+# if sys.version_info[0] < 3:
+#     from StringIO import StringIO # Python 2.x
+# else:
+#     from io import StringIO # Python 3.x
+#
+# bucket_name = 'hist-price'
+# my_bucket = resource.Bucket(bucket_name)
+# files = list(my_bucket.objects.all())
+#
+# for f in files:
+#     file_name = f.key
+#     object_key = file_name
+#     csv_obj = s3.get_object(Bucket=bucket_name, Key=object_key)
+#     body = csv_obj['Body']
+#     csv_string = body.read().decode('utf-8')
+path = "./daily-historical-stock-prices-1970-2018/"
 
-if sys.version_info[0] < 3:
-    from StringIO import StringIO # Python 2.x
-else:
-    from io import StringIO # Python 3.x
+df_price = pd.read_csv(path+'historical_stock_prices.csv', nrows=200)
+df_stock = pd.read_csv(path+'historical_stocks.csv')
 
-bucket_name = 'testing-ming'
-my_bucket = resource.Bucket(bucket_name)
-files = list(my_bucket.objects.all())
 
-for f in files:
-    file_name = f.key
-    object_key = file_name
-    csv_obj = s3.get_object(Bucket=bucket_name, Key=object_key)
-    body = csv_obj['Body']
-    csv_string = body.read().decode('utf-8')
+def s_buy_hold(stocks, start_date, end_date, invest=100, frequency=30):
+    # strategy 1. buy invest amount at start_date, calculate the PnL at end_date
+    pnl = 0
+    if dt.strptime(end_date, "%Y-%m-%d") <= dt.strptime(start_date, "%Y-%m-%d"):
+        return 'You end date is before your start date!'
+    if len(stocks) == 0:
+        return 'You stock ticker is out of scope.'
+    for stock in stocks:
+        select = df_price[df_price['ticker'] == stock][['date', 'adj_close']].copy()
+        if start_date in select['date'].values:
+            p0=select['adj_close'][select['date']==start_date].values[0]
+            if p0 == 0:
+                return "Starting price is zero."
+            else:
+                q = invest/p0
+        else:
+            return 'start date or ticker doesn\'t exist'
+        if end_date in select['date'].values:
+            p1=select['adj_close'][select['date']==end_date].values[0]
+            pnl = q * (p1 - p0)
+        else:
+            return 'end date out of scope.'
+    return pnl
 
-    df = pd.read_csv(StringIO(csv_string))
-    print(df.head())
+
+if __name__ == "__main__":
+    # print(df_price.head())
+    
+    pnl = s_buy_hold(['AHH'], '2013-05-08', '2013-05-14')
+    if type(pnl) is float:
+        print(f'Pnl of your trade is {pnl:.2f}')
+    else:
+        print(pnl)
+
