9a14cba1bab6e3325b2831eef4cb82421f523ce0
Mingyyy <j.yanming@gmail.com>
2019-09-13 12:29:22 -0400

    Initiate dash-test

---
+++ dash-test.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div("MVP - Upgrade")
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
