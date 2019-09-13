d64b8d51f40b0aa3c06533c7a1f313e99c5d2111
Mingyyy <j.yanming@gmail.com>
2019-09-13 17:26:41 -0400

    Add labels for inputs and outputs

--- dash-test.py
+++ dash-test.py
@@ -10,10 +10,36 @@ app = dash.Dash()
 app.layout = html.Div(children=[
     html.H1("MVP - Time to Upgrade"),
     html.Div(children='''
-    symbol to graph:'''),
+    Symbol to graph:'''),
     dcc.Input(id='input', value='', type='text'),
-    html.Div(id='output-graph'),]
+    html.Div(children='''
+    Start Date:'''),
+    dcc.Input(id='input-start', value='', type='text'),
+    html.Div(children='''
+    End Date:'''),
+    dcc.Input(id='input-end', value='', type='text'),
+    html.Div(id='output-graph'),
+    html.Div(children='''
+    PnL for your trade:'''),
+    html.Div(id='output-pnl'),]
 )
+
+
+@app.callback(
+    Output(component_id='output-pnl',component_property='children'),
+    [Input(component_id='input',component_property='value'),
+     Input(component_id='input-start',component_property='value'),
+     Input(component_id='input-end',component_property='value')]
+    )
+def update_pnl(input_data, start_date, end_date):
+    path = "./daily-historical-stock-prices-1970-2018/"
+    df_price = pd.read_csv(path + 'historical_stock_prices.csv', nrows=2000)
+    if start_date and end_date:
+        pnl = s_buy_hold(input_data.upper(), df_price, start_date, end_date)
+        return f'{pnl:.2f}'
+    else:
+        return "Not enough input"
+
 
 @app.callback(
     Output(component_id='output-graph',component_property='children'),
@@ -21,10 +47,10 @@ app.layout = html.Div(children=[
     )
 def update_graph(input_data):
     path = "./daily-historical-stock-prices-1970-2018/"
+    df_price = pd.read_csv(path + 'historical_stock_prices.csv', nrows=2000)
 
-    df_price = pd.read_csv(path + 'historical_stock_prices.csv', nrows=200)
     # df_stock = pd.read_csv(path + 'historical_stocks.csv')
-    selected_ticker = input_data
+    selected_ticker = input_data.upper()
     selected_col1 = 'open'
     selected_col2 = 'close'
 
@@ -46,33 +72,36 @@ def update_graph(input_data):
                   'layout':{
                       'title': f'{selected_ticker} historical data'
                   }
-
     }),
 
 
 
-def s_buy_hold(stocks, df_price, start_date='2014-05-08', end_date='2014-05-14', invest=100, frequency=30):
+def s_buy_hold(stocks, df_price, start_date='2013-05-08', end_date='2013-05-14', invest=100, frequency=30):
     # strategy 1. buy invest amount at start_date, calculate the PnL at end_date
     pnl = 0
     if dt.strptime(end_date, "%Y-%m-%d") <= dt.strptime(start_date, "%Y-%m-%d"):
         return 'You end date is before your start date!'
     if len(stocks) == 0:
         return 'You stock ticker is out of scope.'
-    for stock in stocks:
-        select = df_price[df_price['ticker'] == stock][['date', 'adj_close']].copy()
-        if start_date in select['date'].values:
-            p0=select['adj_close'][select['date']==start_date].values[0]
-            if p0 == 0:
-                return "Starting price is zero."
-            else:
-                q = invest/p0
+
+    if stocks not in df_price['ticker'].values:
+        return 'stock doesn\'t exist'
+    else:
+        select = df_price[df_price['ticker'] == stocks][['date', 'adj_close']].copy()
+
+    if start_date in select['date'].values:
+        p0=select['adj_close'][select['date']==start_date].values[0]
+        if p0 == 0:
+            return "Starting price is zero."
         else:
-            return 'start date or ticker doesn\'t exist'
-        if end_date in select['date'].values:
-            p1=select['adj_close'][select['date']==end_date].values[0]
-            pnl = q * (p1 - p0)
-        else:
-            return 'end date out of scope.'
+            q = invest/p0
+    else:
+        return 'start date doesn\'t exist'
+    if end_date in select['date'].values:
+        p1=select['adj_close'][select['date']==end_date].values[0]
+        pnl = q * (p1 - p0)
+    else:
+        return 'end date out of scope.'
     return pnl
 
 if __name__ == '__main__':
