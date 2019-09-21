38c08101db61085c254ca278675e24f58eb8e9e2
Mingyyy <j.yanming@gmail.com>
2019-09-20 22:52:27 -0400

    Update functions

--- python/from_s3.py
+++ python/from_s3.py
@@ -1,5 +1,6 @@
 import boto3
 
+
 def s3_loader(bucket_name='hist-price'):
     # connect to S3, with boto3: high-level object-oriented API
     s3 = boto3.client('s3')
@@ -9,8 +10,6 @@ def s3_loader(bucket_name='hist-price'):
     # buckets = s3.list_buckets()
     # for bucket in buckets['Buckets']:
     #     print(bucket['CreationDate'].ctime(), bucket['Name'])
-
-    csv_string = []
     my_bucket = resource.Bucket(bucket_name)
     files = list(my_bucket.objects.all())
 
@@ -21,12 +20,23 @@ def s3_loader(bucket_name='hist-price'):
         body = csv_obj['Body']
         body.read().decode('utf-8')
 
+
+def s3_file_sizes(bucket_name):
+    s3 = boto3.client('s3')
+    resource = boto3.resource('s3')
+    my_bucket = resource.Bucket(bucket_name)
+    files = list(my_bucket.objects.all())
+    csv_string = []
+
+    for f in files:
+        object_key = f.key
+        csv_obj = s3.get_object(Bucket=bucket_name, Key=object_key)
         # get the size of the files
         response = s3.head_object(Bucket=bucket_name, Key=object_key)
         size = response['ContentLength']
         csv_string.append(size)
     return csv_string
 
-
 if __name__ == '__main__':
-    print(s3_loader("strategy-upload"))
+    # print(s3_loader("strategy-upload"))
+    print(s3_file_sizes('simulated-bucket'))
