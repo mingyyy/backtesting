20af2337ab305a53d7c6d6274d78b4dd293388cd
Mingyyy <j.yanming@gmail.com>
2019-09-25 22:36:24 -0400

    Update api_18080.py. Functioning version

--- api/api_18080.py
+++ api/api_18080.py
@@ -1,10 +1,37 @@
 import requests
 
 
-path='http://ec2-3-231-23-67.compute-1.amazonaws.com:18080/api/v1/applications/'
-app_id = 'app-20190925195753-0058'
-action = 'jobs'
-path += app_id + '/' + action + '/'
+def check_jobs(app_id, action):
+    path = 'http://ec2-3-231-23-67.compute-1.amazonaws.com:18080/api/v1/applications/'
+    path += app_id + '/' + action + '/'
 
-response = requests.get(path)
-print(response)
+    response = requests.get(path)
+    if response.status_code != 200:
+        return print(response.status_code)
+    else:
+        jobs = response.json()
+        for job in jobs:
+            if job['status'] != 'SUCCEEDED':
+                return print('JobID:', job['jobId'], job['status'])
+        return print('All jobs succeeded!')
+
+
+def check_stages(app_id, action):
+    path = 'http://ec2-3-231-23-67.compute-1.amazonaws.com:18080/api/v1/applications/'
+    path += app_id + '/' + action + '/'
+
+    response = requests.get(path)
+    if response.status_code != 200:
+        return print(response.status_code)
+    else:
+        stages = response.json()
+        for stage in stages:
+            if stage['status'] != 'COMPLETED':
+                str='attemptID: {} failed: {}'.format(stage['attemptId'], stage['failureReason'])
+                return print('StageID:', stage['stageId'], stage['status'], str)
+        return print('All stages completed!')
+
+
+if __name__ == '__main__':
+    check_jobs('app-20190925231336-0062','jobs')
+    check_stages('app-20190925231336-0062', 'stages')
