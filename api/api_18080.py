d37d5973b4a8f6f7a1ce8e7e69f74a84236916d3
Mingyyy <j.yanming@gmail.com>
2019-09-30 13:59:32 -0400

    Add drop down box and rotate the slider text 60deg

--- api/api_18080.py
+++ api/api_18080.py
@@ -13,7 +13,7 @@ def check_jobs(app_id, action):
         for job in jobs:
             if job['status'] != 'SUCCEEDED':
                 return print('JobID:', job['jobId'], job['status'])
-        return print('All jobs succeeded!')
+        return 'ok'
 
 
 def check_stages(app_id, action):
@@ -32,6 +32,28 @@ def check_stages(app_id, action):
         return print('All stages completed!')
 
 
+def check_tasks(app_id, action):
+    path = 'http://ec2-3-231-23-67.compute-1.amazonaws.com:18080/api/v1/applications/'
+    path += app_id + '/' + action + '/'
+
+    response = requests.get(path)
+    if response.status_code != 200:
+        return print(response.status_code)
+    else:
+        tasks = response.json()
+        for task in tasks:
+            print(task)
+        #     if task['status'] != 'COMPLETED':
+        #         str='attemptID: {} failed: {}'.format(task['attemptId'], task['failureReason'])
+        #         return print('StageID:', task['stageId'], task['status'], str)
+        # return print('All stages completed!')
+
+
 if __name__ == '__main__':
-    check_jobs('app-20190925231336-0062','jobs')
-    check_stages('app-20190925231336-0062', 'stages')
+    app_id = 'app-20190930134723-0167'
+    if check_jobs(app_id,'jobs') == 'ok':
+        print('All jobs succeeded!')
+    else:
+        check_stages(app_id, 'stages')
+
+    # check_tasks('app-20190926015654-0064', 'tasks')
--- dashboard/dash/dash-test.py
+++ dashboard/dash/dash-test.py
@@ -42,42 +42,48 @@ external_stylesheets = [
 
 app = dash.Dash(external_scripts=external_scripts,
                 external_stylesheets=external_stylesheets)
-db_string = "postgresql+psycopg2://postgres:{}@ec2-3-229-236-236.compute-1.amazonaws.com:5432/test".format(db_password)
-db_name = "results"
+tbl_string = "postgresql+psycopg2://postgres:{}@ec2-3-229-236-236.compute-1.amazonaws.com:5432/test".format(db_password)
+tbl_name = "sample"
 
 
 def load_data(query):
     conn= psycopg2.connect(host='ec2-3-229-236-236.compute-1.amazonaws.com', user='postgres', password=db_password)
     sql_command = (query)
-
     df_price = pd.read_sql(sql_command, conn)
     return df_price
 
 # Step 1. Load Data from Postgres
-query = "SELECT * FROM {} ORDER BY purchase_date;".format(db_name)
+query = "SELECT sector, ticker, purchase_date ,purchase_price FROM {} WHERE ticker='SIBBHM' ORDER BY purchase_date;".format(tbl_name)
 df_price = load_data(query)
 
 # Step 2. Define Range Slider options
-query = "SELECT purchase_date FROM {} ORDER BY purchase_date;".format(db_name)
+query = "SELECT purchase_date FROM {} group by purchase_date ORDER BY purchase_date;".format(tbl_name)
 dates = load_data(query)
 start_date = dates['purchase_date'].min()
 end_date = dates['purchase_date'].max()
 
 N = int(dates.count()[0])
-# print(N)
 date_list = dates['purchase_date'].tolist()
+step = int(N/18)
+date_mark = {i: {'label': dates.iloc[i][0].strftime('%Y-%m'), 'style': {'transform': 'rotate(-60deg)'}}
+             for i in range(0, N+step, step) if i <= N}
 
-date_mark = {i: dates.iloc[i][0].strftime('%Y-%m') for i in range(0,N,20)}
-# print(date_mark)
+# Initiate Graph
+trace_1 = go.Scatter(x=df_price.purchase_date, y=df_price['purchase_price'],
+                    name='Purchase Price',
+                    line=dict(width=2,
+                                color='rgb(229, 151, 50)'),
+                     mode='markers')
+layout = go.Layout(title='Time Series Plot',
+                   hovermode='closest')
+fig = go.Figure(data=[trace_1], layout=layout)
 
-trace_1 = go.Scatter(x = df_price.purchase_date, y = df_price['purchase_price'],
-                    name = 'Purchase Price',
-                    line = dict(width = 2,
-                                color = 'rgb(229, 151, 50)'),
-                     mode = 'markers')
-layout = go.Layout(title = 'Time Series Plot',
-                   hovermode = 'closest')
-fig = go.Figure(data = [trace_1], layout = layout)
+# Step 3. Define dropdown list.
+q = "SELECT sector, ticker FROM {} group by sector, ticker limit 10;".format(tbl_name)
+ticker_list = load_data(q)['ticker']
+
+opts = [{'label': 'stock ' + i, 'value': i} for i in ticker_list]
+
 
 
 # Step 4. Create a Dash layout
@@ -86,13 +92,45 @@ app.layout = html.Div(
         # Header and some styles
         html.Div([
             html.H1("Trading Strategy Back Testing Dashboard"),
-            html.P("MVP testing version 0.1"),],
+            html.P("MVP testing version 1.1"),],
             style={'padding': '20px',
-                   'backgroundColor': '#74F3FF'}),
-        #Input field
-        html.Div(children='''
-        Symbol to graph:'''),
-        dcc.Input(id='input', value='', type='text'),
+                   'backgroundColor': '#74F3FF'
+                    }),
+
+        # Input field
+        # html.Div(children='''
+        # Symbol to graph:'''),
+        # dcc.Input(id='input', value='', type='text'),
+
+        # Dropdown 1
+        html.P([
+                    # html.Label("Choose a Ticker"),
+                    dcc.Dropdown(id='opt',
+                                 options=opts,
+                                 value=opts[0]['value'],
+                                placeholder="Select a ticker",
+                                 )
+                ], style={'width': '300px',
+                        'fontSize': '15px',
+                        'padding-left': '30px',
+                        'display': 'inline-block'}),
+
+        # Dropdown 2
+        # html.P([
+        #     html.Label("Want to have less ticker"),
+        #     dcc.Dropdown(id='opt_sector',
+        #                  options=opts,
+        #                  value=[opts[0], opts[1]],
+        #                  placeholder="Select a Sector",
+        #                  multi = True
+
+        #                  )
+        # ], style={'width': '200px',
+        #           'fontSize': '20px',
+        #           'padding-left': '30px',
+        #           'display': 'inline-block'}),
+
+
 
         # Add a plot
         dcc.Graph(id='output-graph', figure=fig),
@@ -108,73 +146,44 @@ app.layout = html.Div(
         ], style={'width': '80%',
                   'fontSize': '20px',
                   'padding-left': '100px',
-                  'display': 'inline-block'})
+                  'display': 'inline-block',
+                  # 'transform': 'rotate(-60deg)',
+                  })
     ]
 )
 
+
+
 # Step 4. Add callback functions to render the page
 @app.callback(Output('output-graph', 'figure'),
-             [Input('input', 'value'),Input('slider', 'value')])
+             [Input('opt', 'value'), Input('slider', 'value')])
 def update_figure(input1, input2):
+    selected_ticker = input1[0]
+    print(selected_ticker)
 
-    query = "SELECT purchase_date FROM {} ORDER BY purchase_date;".format(db_name)
+    query = "SELECT purchase_date FROM {} WHERE TICKER='{}' ORDER BY purchase_date;".format(tbl_name, selected_ticker)
     dates = load_data(query)
+    print(dates.iloc[input2[0]][0], dates.iloc[input2[1]][0])
 
     # filtering the data
 
     st2 = df_price[(df_price.purchase_date > dates.iloc[input2[0]][0]) & (df_price.purchase_date < dates.iloc[input2[1]][0])]
 
     # updating the plot
-    trace_1 = go.Scatter(x = st2.purchase_date, y = st2['purchase_price'],
-                        name = 'Stock purchase',
-                        line = dict(width = 2,
-                                    color = 'rgb(229, 151, 50)',),
+    trace_1 = go.Scatter(x=st2.purchase_date, y=st2['purchase_price'],
+                        name='Stock purchase',
+                        line = dict(width=1,
+                                    color='rgb(229, 151, 50)',),
                          mode = 'markers')
-    selected_ticker = input1.upper()
-    trace_2 = go.Scatter(x = st2.purchase_date, y = st2['purchase_price'],#[df_price['ticker'] == selected_ticker],
+
+    trace_2 = go.Scatter(x = st2.purchase_date, y =selected_ticker,
                         name = input1,
-                        line = dict(width = 2,
-                                    color = 'rgb(106, 181, 135)',),
+                        line = dict(width=1,
+                                    color='rgb(106, 181, 135)',),
                         mode='markers'
                         )
     fig = go.Figure(data=[trace_1, trace_2], layout=layout)
     return fig
-
-
-# @app.callback(
-#     Output(component_id='output-graph',component_property='children'),
-#     [Input(component_id='input',component_property='value')]
-#     )
-# def update_graph(input_data):
-#     query = "SELECT * FROM {};".format(db_name)
-#     df_price = load_data(query)
-#     selected_ticker = input_data.upper()
-#
-#     selected_col1 = 'purchase_price'
-#     selected_col2 = 'pnl'
-#
-#     # list_purchase = df_price[selected_col1][df_price['ticker'] == selected_ticker]
-#     # list_pnl = df_price[selected_col2][df_price['ticker'] == selected_ticker]
-#     # list_date = df_price['purchase_date'][df_price['ticker'] == selected_ticker]
-#     list_purchase = df_price[selected_col1]
-#     list_pnl = df_price[selected_col2]
-#     list_date = df_price['purchase_date']
-#
-#     return dcc.Graph(id='example',
-#               figure={
-#                   'data':[{'x':list_date,
-#                            'y':list_purchase,
-#                            'type':'line',
-#                            'name': selected_col1},
-#                           {'x': list_date,
-#                            'y': list_pnl,
-#                            'type': 'bar',
-#                            'name': selected_col2}
-#                           ],
-#                   'layout':{
-#                       'title': '{} historical data'.format(selected_ticker)
-#                   }
-#     })
 
 
 
