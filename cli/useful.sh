8a2942ecd52c0a90116e81287a97db00446611bc
Mingyyy <j.yanming@gmail.com>
2019-10-03 09:04:41 -0400

    Add useful command lines

---
+++ cli/useful.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# environment variables
+export PYSPARK_PYTHON=python3;
+export SPARK_CLASSPATH= <path to downloaded jar>/postgresql-42.2.8.jar
+
+# spark shell
+pyspark
+
+# copy files to
+scp -i ~/.ssh/my-IAM-keypair.pem <schema file>  ubuntu@<public DNS>:/home/ubuntu/
+
+
+# create schema in DB EC2
+sudo psql -U postgres -d postgres -a -f schema.sql
+
+
+# check file sizes in AWS S3 bucket
+aws s3 ls --summarize --human-readable --recursive s3://bucket-name/directory
+
+# how to start history server in master node
+cd /usr/local/spark/sbin &&  ./start-history-server.sh
+
+# run spark from terminal
+spark-submit --master spark://<private DNS>:7077  --jars <path to jar>/postgresql-42.2.8.jar  <path to file>
+
